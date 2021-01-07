c990be268a6207bcc236062d9373ce3158b1aac6
Wiktor Kulczyk <wiktorio04@outlook.com>
2021-01-07 20:18:46 +0100

    testing

--- src/main/java/com/antiafk/AntiAFK.java
+++ src/main/java/com/antiafk/AntiAFK.java
@@ -1,5 +1,6 @@
 package com.antiafk;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AntiAFK extends JavaPlugin {
@@ -7,11 +8,12 @@ public final class AntiAFK extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        
+    getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[AntiAFK] Plugin is enabled");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getServer().getConsoleSender().sendMessage(ChatColor.RED + "[AntiAFK] Plugin is disabled");
     }
 }
