b5039425776946c3109e026f115b71fe2ace8e8a
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-01-07 20:30:14 +0100

    Upgrade <ExamleTest.java>

--- src/test/java/com/antiafk/ExampleTest.java
+++ src/test/java/com/antiafk/ExampleTest.java
@@ -1,9 +1,14 @@
 package com.antiafk;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.junit.Assert.*;
+import static org.junit.Assert.*;
 
 @RunWith(JUnit4.class)
 public class ExampleTest {
+    @Test
+    public void testExample(){
+        assertTrue(true);
+    }
 }
