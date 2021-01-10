3e6d953d0f9f9674660945ef76c8279b41bbb125
Wiktor Kulczyk <wiktorio04@outlook.com>
2021-01-10 15:37:08 +0100

    getPlayer()

--- src/main/java/com/antiafk/AntiAFK.java
+++ src/main/java/com/antiafk/AntiAFK.java
@@ -1,10 +1,16 @@
 package com.antiafk;
 
 import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AntiAFK extends JavaPlugin {
-
     @Override
     public void onEnable() {
         // Plugin startup logic
@@ -16,4 +22,14 @@ public final class AntiAFK extends JavaPlugin {
         // Plugin shutdown logic
         getServer().getConsoleSender().sendMessage(ChatColor.RED + "[AntiAFK] Plugin is disabled");
     }
+
 }
+class Join implements Listener {
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent ev) {
+        ev.setJoinMessage("Witamy");
+        Player player = ev.getPlayer();
+    }
+}
+
+
