da3a407bcfb3923d1ca5f8694cb422a2db32568d
Wiktor Kulczyk <wiktorio04@outlook.com>
2021-01-10 15:48:30 +0100

    first versim

--- src/main/java/com/antiafk/AntiAFK.java
+++ src/main/java/com/antiafk/AntiAFK.java
@@ -1,6 +1,8 @@
 package com.antiafk;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Server;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -11,10 +13,12 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AntiAFK extends JavaPlugin {
+    private static Server server;
     @Override
     public void onEnable() {
         // Plugin startup logic
         getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[AntiAFK] Plugin is enabled");
+        server = getServer();
     }
 
     @Override
@@ -22,13 +26,18 @@ public final class AntiAFK extends JavaPlugin {
         // Plugin shutdown logic
         getServer().getConsoleSender().sendMessage(ChatColor.RED + "[AntiAFK] Plugin is disabled");
     }
-
+    public static void AFK(Player player) {
+        double oldX = player.getLocation().getX();
+        double oldZ = player.getLocation().getZ();
+        server.getConsoleSender().sendMessage(ChatColor.AQUA + "X: " + oldX + "Z: " + oldZ);
+    }
 }
 class Join implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent ev) {
-        ev.setJoinMessage("Witamy");
+        ev.setJoinMessage("Witamy na serwerze!");
         Player player = ev.getPlayer();
+        AntiAFK.AFK(player);
     }
 }
 
