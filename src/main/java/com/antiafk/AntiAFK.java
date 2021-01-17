d1654a4cab0eccc1528e2077fd800e005cf64706
Wiktor Kulczyk <wiktorio04@outlook.com>
2021-01-17 17:25:15 +0100

    First working version

--- src/main/java/com/antiafk/AntiAFK.java
+++ src/main/java/com/antiafk/AntiAFK.java
@@ -1,14 +1,21 @@
 package com.antiafk;
 import com.antiafk.events.AntiAFKEvents;
 import org.bukkit.ChatColor;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
 
 public class AntiAFK extends JavaPlugin{
+    static JavaPlugin plugin;
+    public static JavaPlugin getMainPlugin() {
+        return plugin;
+    }
     @Override
     public void onEnable() {
         // Plugin startup logic
-        getServer().getPluginManager().registerEvents(new AntiAFKEvents(), this);
+        plugin = this;
         getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[AntiAFK] Plugin is enabled");
+        getServer().getPluginManager().registerEvents(new AntiAFKEvents(), this);
     }
     @Override
     public void onDisable() {
--- src/main/java/com/antiafk/events/AntiAFKEvents.java
+++ src/main/java/com/antiafk/events/AntiAFKEvents.java
@@ -1,37 +1,35 @@
 package com.antiafk.events;
 
+import com.antiafk.AntiAFK;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-
-class PrimeThread extends Thread {
-    Player player;
-    PrimeThread(Player player) {
-        this.player = player;
-    }
-    public void run() {
-        while (player.isOnline()) {
-            try {
-                PrimeThread.sleep(20000);
-                double oldX = player.getLocation().getX();
-                double oldZ = player.getLocation().getZ();
-                if(player.getLocation().getX() == oldX && player.getLocation().getZ() == oldZ ) {
-                    player.kickPlayer(ChatColor.RED + "Zostałeś wyrzucony za bycie AFK");
-                }
-            } catch (InterruptedException ie) {
-
-            }
-        }
-    }
-}
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitScheduler;
+import org.bukkit.scheduler.BukkitTask;
 
 public class AntiAFKEvents implements Listener {
     @EventHandler
-    public static void onPlayerJoin(PlayerJoinEvent event) {
+    public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        PrimeThread thread = new PrimeThread(player);
-        thread.start();
+        BukkitTask task = new BukkitRunnable() {
+            double oldX;
+            double oldZ;
+            public void run() {
+                if (player.isOnline()) {
+                    if (player.getLocation().getX() == oldX && player.getLocation().getZ() == oldZ) {
+                        player.kickPlayer(ChatColor.RED + "Zostałeś wyrzucony za bycie AFK");
+                    } else {
+                        oldX = player.getLocation().getX();
+                        oldZ = player.getLocation().getZ();
+                    }
+                } else {
+                    this.cancel();
+                }
+            }
+        }.runTaskTimer(AntiAFK.getMainPlugin(), 30 * 20, 30 * 20);
     }
 }
