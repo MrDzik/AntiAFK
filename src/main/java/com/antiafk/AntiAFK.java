3a0b55a376b13999b33167c2cead7c27696bef5e
Wiktor Kulczyk <wiktorio04@outlook.com>
2021-01-17 15:02:05 +0100

    PrimeThread added

--- src/main/java/com/antiafk/AntiAFK.java
+++ src/main/java/com/antiafk/AntiAFK.java
@@ -8,7 +8,7 @@ public class AntiAFK extends JavaPlugin{
     public void onEnable() {
         // Plugin startup logic
         getServer().getPluginManager().registerEvents(new AntiAFKEvents(), this);
-        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[AntiAFK] Plugin is enabled");
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[AntiAFK] Plugin is enaaabled");
     }
     @Override
     public void onDisable() {
--- src/main/java/com/antiafk/events/AntiAFKEvents.java
+++ src/main/java/com/antiafk/events/AntiAFKEvents.java
@@ -6,23 +6,32 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+class PrimeThread extends Thread {
+    Player player;
+    PrimeThread(Player player) {
+        this.player = player;
+    }
+    public void run() {
+        while (player.isOnline()) {
+            try {
+                double oldX = player.getLocation().getX();
+                double oldZ = player.getLocation().getZ();
+                PrimeThread.sleep(10000);
+                if(player.getLocation().getX() == oldX && player.getLocation().getZ() == oldZ ) {
+                    player.kickPlayer(ChatColor.RED + "Zostałeś wyrzucony za bycie AFK");
+                }
+            } catch (InterruptedException ie) {
+
+            }
+        }
+    }
+}
 
 public class AntiAFKEvents implements Listener {
     @EventHandler
     public static void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        while (true) {
-            double oldX = player.getLocation().getX();
-            double oldZ = player.getLocation().getZ();
-            long startTime = System.currentTimeMillis();
-            for (int count = 0; ;count++) {
-                long now = System.currentTimeMillis();
-                if(now - startTime >= 10000) break;
-            }
-            if(player.getLocation().getX() == oldX && player.getLocation().getZ() == oldZ ) {
-                player.kickPlayer(ChatColor.RED + "Zostałeś wyrzucony za bycie AFK");
-                return;
-            }
-        }
+        PrimeThread thread = new PrimeThread(player);
+        thread.run();
     }
 }
