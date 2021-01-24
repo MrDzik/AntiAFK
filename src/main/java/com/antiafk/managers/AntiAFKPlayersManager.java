b18c520a49178405c097ab5152086f78e671aea4
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-01-24 19:03:20 +0100

    elimination of repetitions

--- src/main/java/com/antiafk/managers/AntiAFKPlayersManager.java
+++ src/main/java/com/antiafk/managers/AntiAFKPlayersManager.java
@@ -4,28 +4,30 @@ import com.antiafk.AntiAFK;
 import com.antiafk.runnables.AntiAFKThread;
 import com.antiafk.objects.PlayerData;
 import org.bukkit.entity.Player;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class AntiAFKPlayersManager {
     public List<PlayerData> PlayerList1 = new ArrayList<>();
     public List<PlayerData> PlayerList2 = new ArrayList<>();
-    private List<PlayerData> getPlayerListWithLowerSize(){
-        return (PlayerList1.size() <= PlayerList2.size()) ? PlayerList1 : PlayerList2;
+
+    //TODO find better name
+    private List<PlayerData> getFirstOrSecondList(boolean bool) {
+        return bool? PlayerList1 : PlayerList2;
     }
     public void addPlayer(Player player) {
         PlayerData playerData = new PlayerData(player);
-        getPlayerListWithLowerSize().add(playerData);
+        boolean isList1Smaller = PlayerList1.size() <= PlayerList2.size();
+        getFirstOrSecondList(isList1Smaller).add(playerData);
     }
     public void deletePlayer(PlayerData player) {
-        if (PlayerList1.contains(player)) {
-            PlayerList1.remove(player);
-        } else {
-            PlayerList2.remove(player);
-        }
+        boolean isInList1 = PlayerList1.contains(player);
+        getFirstOrSecondList(isInList1).remove(player);
     }
+
     public void run() {
-        new AntiAFKThread(1).runTaskTimerAsynchronously(AntiAFK.getMainPlugin(), 0, 180 * 20);
-        new AntiAFKThread(2).runTaskTimerAsynchronously(AntiAFK.getMainPlugin(), 5 * 20, 180 * 20);
+        new AntiAFKThread(1).runTaskTimer(AntiAFK.getMainPlugin(), 0, 180 * 20);
+        new AntiAFKThread(2).runTaskTimer(AntiAFK.getMainPlugin(), 5 * 20, 180 * 20);
     }
 }
