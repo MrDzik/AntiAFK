18a5df189b9320bd6b078d8f2c09cad5385daaab
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-01-29 12:43:55 +0100

    supplement class Config

--- src/main/java/com/antiafk/objects/Config.java
+++ src/main/java/com/antiafk/objects/Config.java
@@ -1,4 +1,17 @@
 package com.antiafk.objects;
 
 public class Config {
+    private long timeToKick;
+
+    public long getTimeToKick() {
+        return this.timeToKick;
+    }
+    public void setTimeToKick(long timeToKick) {
+        this.timeToKick = timeToKick;
+    }
+    public static Config getDefault(){
+        Config config = new Config();
+        config.setTimeToKick(180);
+        return config;
+    }
 }
