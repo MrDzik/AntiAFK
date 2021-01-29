44f074432b074acbf4087f95c02fbc4bb0192a15
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-01-29 12:47:30 +0100

    supplement class ConfigReader

--- src/main/java/com/antiafk/objects/ConfigReader.java
+++ src/main/java/com/antiafk/objects/ConfigReader.java
@@ -1,4 +1,33 @@
 package com.antiafk.objects;
 
+import org.json.simple.JSONObject;
+import java.io.FileReader;
+import org.json.simple.parser.JSONParser;
+
 public class ConfigReader {
+    private final String PATH = "";
+
+    private JSONObject readJSON() throws Exception {
+        JSONParser parser = new JSONParser();
+        try {
+            FileReader fileReader = new FileReader(PATH);
+            JSONObject jsonObject = (JSONObject)parser.parse(fileReader);
+            return jsonObject;
+        }
+        catch(Exception e) {
+            throw e;
+        }
+    }
+    public Config readOrReturnDefault(){
+        try {
+            JSONObject jsonObject = readJSON();
+            Config config = new Config();
+            long timeToKick = (long)jsonObject.get("TimeToKick");
+            config.setTimeToKick(timeToKick);
+            return config;
+        }
+        catch (Exception e){
+            return Config.getDefault();
+        }
+    }
 }
